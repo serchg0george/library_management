1aea0a40ee3d71d9557511c6b8f6f3453876b228
serchg0george <serchg0george@gmail.com>
2025-02-11 13:16:52 +0200

    Refactored dto to record

--- src/main/java/com/evolvestech/library_management/dtos/BookDto.java
+++ src/main/java/com/evolvestech/library_management/dtos/BookDto.java
@@ -2,34 +2,24 @@ package com.evolvestech.library_management.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-public class BookDto extends BaseDto {
+public record BookDto(Long id,
+                      @NotBlank String title,
 
-    @NotBlank
-    private String title;
+                      @NotBlank String author,
 
-    @NotBlank
-    private String author;
+                      @NotBlank
+                      @Length(max = 10)
+                      @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Input date should be in format YYYY-MM-DD")
+                      String publicationDate,
 
-    @NotBlank
-    @Length(max = 10)
-    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Input date should be in format YYYY-MM-DD")
-    private String publicationDate;
+                      @NotBlank
+                      String genre,
 
-    @NotBlank
-    private String genre;
-
-    @NotBlank
-    @Length(max = 13)
-    @Pattern(regexp = "^\\d{10}(\\d{3})?$", message = "Input must be either 10 or 13 digits")
-    private String isbn;
+                      @NotBlank
+                      @Length(max = 13)
+                      @Pattern(regexp = "^\\d{10}(\\d{3})?$", message = "Input must be either 10 or 13 digits")
+                      String isbn
+) {
 }
