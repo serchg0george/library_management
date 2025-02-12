b8a1e5a7fe5744a507c2162b95e4829397829047
serchg0george <serchg0george@gmail.com>
2025-02-12 12:22:18 +0200

    Created User entity with Role enum for it

---
+++ src/main/java/com/evolvestech/library_management/entities/Role.java
@@ -0,0 +1,6 @@
+package com.evolvestech.library_management.entities;
+
+public enum Role {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/evolvestech/library_management/entities/User.java
@@ -0,0 +1,70 @@
+package com.evolvestech.library_management.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "t_users")
+public class User extends BaseEntity implements UserDetails {
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
