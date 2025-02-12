27bdacd1743a437c6dfc0ad8b87e4b0433d8b750
serchg0george <serchg0george@gmail.com>
2025-02-12 12:22:41 +0200

    Created UserRepo for user

---
+++ src/main/java/com/evolvestech/library_management/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.evolvestech.library_management.repositories;
+
+import com.evolvestech.library_management.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
