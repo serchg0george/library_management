6a7aaa82e8479235caa517d1595aa2aabb65d3fb
serchg0george <serchg0george@gmail.com>
2025-02-16 21:40:03 +0200

    Added autofill samples for t_books

---
+++ src/main/resources/data.sql
@@ -0,0 +1,23 @@
+TRUNCATE TABLE t_books;
+
+INSERT INTO t_books (id, title, author, publication_date, genre, isbn)
+VALUES (1, 'To Kill a Mockingbird', 'Harper Lee', '1960-07-11', 'Classic', '9780446310789'),
+       (2, '1984', 'George Orwell', '1949-06-08', 'Dystopian', '9780451524935'),
+       (3, 'The Great Gatsby', 'F. Scott Fitzgerald', '1925-04-10', 'Classic', '9780743273565'),
+       (4, 'Pride and Prejudice', 'Jane Austen', '1813-01-28', 'Romance', '9780141439518'),
+       (5, 'The Catcher in the Rye', 'J.D. Salinger', '1951-07-16', 'Coming-of-Age', '9780316769488'),
+       (6, 'The Hobbit', 'J.R.R. Tolkien', '1937-09-21', 'Fantasy', '9780547928227'),
+       (7, 'Harry Potter and the Philosopher''s Stone', 'J.K. Rowling', '1997-06-26', 'Fantasy', '9780747532743'),
+       (8, 'The Lord of the Rings', 'J.R.R. Tolkien', '1954-07-29', 'Fantasy', '9780618640157'),
+       (9, 'Animal Farm', 'George Orwell', '1945-08-17', 'Political Satire', '9780451526342'),
+       (10, 'Brave New World', 'Aldous Huxley', '1932-01-01', 'Dystopian', '9780060850524'),
+       (11, 'The Diary of a Young Girl', 'Anne Frank', '1947-06-25', 'Biography', '9780553296983'),
+       (12, 'The Alchemist', 'Paulo Coelho', '1988-01-01', 'Adventure', '9780062315007'),
+       (13, 'The Da Vinci Code', 'Dan Brown', '2003-03-18', 'Mystery', '9780307474278'),
+       (14, 'The Hunger Games', 'Suzanne Collins', '2008-09-14', 'Dystopian', '9780439023481'),
+       (15, 'The Girl with the Dragon Tattoo', 'Stieg Larsson', '2005-08-23', 'Mystery', '9780307269751'),
+       (16, 'The Shining', 'Stephen King', '1977-01-28', 'Horror', '9780307743657'),
+       (17, 'Gone with the Wind', 'Margaret Mitchell', '1936-06-30', 'Historical Fiction', '9781416548942'),
+       (18, 'The Book Thief', 'Markus Zusak', '2005-03-14', 'Historical Fiction', '9780375831003'),
+       (19, 'The Road', 'Cormac McCarthy', '2006-09-26', 'Post-Apocalyptic', '9780307387899'),
+       (20, 'Life of Pi', 'Yann Martel', '2001-09-11', 'Adventure', '9780156027328');
